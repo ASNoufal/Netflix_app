5c320b7a82949dc48b65ee183c14cd59e16d88de
Naufal As <naufal202@gmail.com>
2023-09-17 11:52:41 +0530

    change int eh new & hot page

--- lib/presentation/news & hot/screen_news.dart
+++ lib/presentation/news & hot/screen_news.dart
@@ -5,8 +5,62 @@ class NewsandHot extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("News and hot"),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: kDefaultIconDarkColor,
+          title: const Text(
+            "New & Hot",
+            style: TextStyle(color: kDefaultIconLightColor),
+          ),
+          actions: [
+            const Icon(
+              Icons.cast,
+              size: 40,
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                color: Colors.blue,
+                width: 30,
+                height: 30,
+              ),
+            ),
+          ],
+          bottom: const TabBar(
+              indicatorSize: TabBarIndicatorSize.tab,
+              indicatorColor: Colors.black,
+              automaticIndicatorColorAdjustment: false,
+              indicator: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(30))),
+              labelColor: kDefaultIconDarkColor,
+              unselectedLabelColor: kDefaultIconLightColor,
+              tabs: [
+                Tab(
+                  text: "🍿 Comming Soon",
+                ),
+                Tab(text: "👀 Everyone's Watching")
+              ]),
+        ),
+        body: TabBarView(children: [commingSoon(), everyoneWatching()]),
+      ),
     );
   }
+}
+
+everyoneWatching() {
+  return Container(
+    child: Text("Every one waching"),
+  );
+}
+
+commingSoon() {
+  return Container(
+    child: Text("comming soon"),
+  );
 }
